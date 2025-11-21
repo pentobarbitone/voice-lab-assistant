0169c1c25658a2edb9db7e9cd8d9c7b711b36694
Katie Wei <sakurieblossie@gmail.com>
2025-11-21 10:09:15 -0500

    main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from assistant_core import VoiceAssistant
+
+def main():
+    assistant = VoiceAssistant(name="Aurora")  # or any name you like
+    assistant.run()
+
+if __name__ == "__main__":
+    main()
